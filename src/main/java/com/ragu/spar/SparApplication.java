5ed17976ff7784b0de9a566a01e7c1a917eb031a
Nana Peprah <nanakwesi.peprah@gmail.com>
2015-08-23 02:06:15 -0400

    Added RAML Spec

--- src/main/java/com/ragu/spar/SparApplication.java
+++ src/main/java/com/ragu/spar/SparApplication.java
@@ -3,6 +3,7 @@ package com.ragu.spar;
 import com.ragu.spar.resources.GameResource;
 import com.ragu.spar.resources.HelloWorldResource;
 import io.dropwizard.Application;
+import io.dropwizard.assets.AssetsBundle;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 
@@ -14,11 +15,13 @@ public class SparApplication extends Application<SparConfiguration> {
 
     @Override
     public void initialize(Bootstrap<SparConfiguration> bootstrap) {
+        bootstrap.addBundle(new AssetsBundle("/dist", "/", "index.html"));
     }
 
     @Override
     public void run(SparConfiguration configuration, Environment environment) throws Exception {
         environment.jersey().register(new HelloWorldResource());
         environment.jersey().register(new GameResource());
+        environment.jersey().setUrlPattern("/api/v1/*");
     }
 }
